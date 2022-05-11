ba5ea5078477215b2c77b7e632d608a5284e2a04
swarup <swarup260@gmail.com>
2022-05-11 23:12:56 +0530

    [API] eventemitter playground / experiment code added

--- API/src/events/console.event.js
+++ API/src/events/console.event.js
@@ -1,13 +1,72 @@
-const { EventEmitter } = require("node:events")
+// const { EventEmitter } = require("node:events")
 
-class Console extends EventEmitter {
+// class Console extends EventEmitter {
 
-}
+//     constructor(...params) {
+//         super(...params)
+//         this.middleware = []
+//     }
 
-const consoleEmitter = new Console()
+//     use(fn) {
+//         if (!(typeof fn == "function")) throw new Error("Middleware Must be Function !")
+//         this.middleware.push(fn)
+//     }
+
+//     emit(...params) {
+//         const args = params[1]
+//         this.fnMiddleware(args).then(super.emit(...params)).catch(err => { throw new Error(err) })
+//     }
+
+//     fnMiddleware(args) {
+//         const dispatch = () => { }
+//         this.middleware.forEach(fn => {
+//             fn(args, dispatch)
+//         })
+
+//         return Promise.resolve()
+//     }
+
+// }
+
+// const consoleEmitter = new Console()
 
 
-consoleEmitter.on("print", (val) => console.log(val))
+
+// consoleEmitter.use((context, next) => {
+//     context.messageBody = Buffer.from(context.messageBody, "base64").toString("ascii")
+//     context.dateTime = new Date(),
+//         context.userID = 1
+//     return
+// })
 
 
-module.exports = consoleEmitter
+// consoleEmitter.on("SAVE_MESSAGE", (val) => console.log(val))
+
+
+
+// consoleEmitter.emit("SAVE_MESSAGE", { messageBody: Buffer.from("Hello World").toString('base64') })
+
+
+// module.exports = consoleEmitter
+
+
+
+
+// // console.log(Buffer.from(iv,"hex").toString('hex'))
+
+// const message = "HELLO WORLD"
+
+// const cipher = crypto.createCipheriv("aes-192-ccm", Buffer.from("password"), iv, {
+//     authTagLength: 16
+// })
+
+// cipher.setAAD(Buffer.from("0123456789","hex"),{
+//     plaintextLength : Buffer.byteLength(message)
+// })
+
+// const cipherText = cipher.update(message, "utf8")
+// cipher.final()
+
+// console.log(cipherText)
+
+
