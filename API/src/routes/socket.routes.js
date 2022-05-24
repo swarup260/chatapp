b0250f26c527c991b5deca09b38c777fdff98f6a
Swarup Das <swarup.das@airpay.com>
2022-05-24 16:57:25 +0530

    [API] event added

--- API/src/routes/socket.routes.js
+++ API/src/routes/socket.routes.js
@@ -19,17 +19,6 @@ module.exports = function socketRoute(io) {
     io.on("connection", socket => {
 
         console.log("USER CONNECTED ON MAIN CHANNEL")
-
-
-        socket.on("JOIN_ROOM",({ room,userID  }) => {
-            rooms.push(room)
-            io.socketsJoin(room)
-            users.push({ room,userID  })
-            console.log({room,userID})
-        })
-
-        socket.on("CREATE_NEW_ROOM",val => console.log(val))
-        socket.on("JOIN_ROOM",val => console.log(val))
         socket.on("SEND_MESSAGE",val => socket.broadcast.emit("RECIEVE_MESSAGE",val))
 
     })
