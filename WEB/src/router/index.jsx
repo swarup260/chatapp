5c8874a1136b2b7c5b279009ef8c162a6abed8e9
Swarup Das <swarup.das@airpay.com>
2022-05-30 19:07:13 +0530

    [WEB] lazy implemented in routing

--- WEB/src/router/index.jsx
+++ WEB/src/router/index.jsx
@@ -1,18 +1,29 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import * as React from "react";
 import App from "../App";
-import SignUp from "../pages/Signup";
 import NotFound from '../pages/NotFound';
 import RequireAuth from "../components/RequireAuth";
-import Home from "../pages/Home";
+
 
 export default function AppRouter() {
+
+  const SignUp = React.lazy(() => import("../pages/Signup"))
+  const Home = React.lazy(() => import("../pages/Home"))
+  const NotFound = React.lazy(() => import("../pages/NotFound"))
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/home" element={<RequireAuth><Home /></RequireAuth>} />
-        <Route path="*" element={<NotFound/> }/>
+        <Route path="/sign-up" element={<React.Suspense fallback={<>...</>}>
+          <SignUp />
+        </React.Suspense>} />
+        <Route path="/home" element={<React.Suspense fallback={<>...</>}>
+          <RequireAuth><Home /></RequireAuth>
+        </React.Suspense>} />
+        <Route path="*" element={<React.Suspense fallback={<>...</>}>
+          <NotFound />
+        </React.Suspense>} />
       </Routes>
     </BrowserRouter>
   );
