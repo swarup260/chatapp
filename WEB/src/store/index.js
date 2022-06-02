684d2be4c0e009d9e6c345e41813b5b836405a82
swarup <swarup260@gmail.com>
2022-06-02 23:13:56 +0530

    [WEB] middleware added

--- WEB/src/store/index.js
+++ WEB/src/store/index.js
@@ -10,15 +10,16 @@ import {
     REGISTER,
 } from 'redux-persist';
 import { SET_SOCKET } from "./socket";
+import socketMiddleware from "./middlewares/socket.middleware";
 
 export default function () {
     return configureStore({
         reducer,
         middleware: (getDefaultMiddleware) => getDefaultMiddleware({
             serializableCheck: {
-                ignoredActions: [SET_SOCKET.type,FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+                ignoredActions: [SET_SOCKET.type, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
             }
             // serializableCheck: false
-        })
+        }).concat(socketMiddleware)
     })
 }
