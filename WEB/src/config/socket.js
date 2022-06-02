705c6c5ef90a9350ced1422203035d9a31addb5a
Swarup Das <swarup.das@airpay.com>
2022-06-02 18:19:31 +0530

    [WEB] socket channel added

--- WEB/src/config/socket.js
+++ WEB/src/config/socket.js
@@ -1,21 +1,8 @@
 import { io } from "socket.io-client";
 
-export const channels = {
-    NOTIFICATION: "notification"
-}
-
-export const socketEvent = {
-    SEND_MESSAGE: 'SEND_MESSAGE',
-    RECIEVE_MESSAGE: 'RECIEVE_MESSAGE',
-    BOARDCAST_MESSAGE: 'BOARDCAST_MESSAGE',
-    USER_CONNECTED: 'USER_CONNECTED',
-    CREATE_NEW_ROOM: 'CREATE_NEW_ROOM',
-    JOIN_ROOM: "JOIN_ROOM",
-    NEW_JOIN:"NEW_JOIN"
-}
-
-
 /* socket.io example */
 /* intialize Socket Instance */
 export const socket = io(`http://${window.location.hostname}:5000`);
-// export const notificationSocket = io(`http://${window.location.hostname}:5000/notification`);
+export const messageSocket = io(`http://${window.location.hostname}:5000/message`);
+export const chatSocket = io(`http://${window.location.hostname}:5000/chat`);
+export const notificationSocket = io(`http://${window.location.hostname}:5000/notification`);
